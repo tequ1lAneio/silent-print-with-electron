f96e47b716f3638bef0ddbe67c945d6b697d4e08
tequ1lAneio <evanlgp@hotmail.com>
2019-06-20 10:19:48 +0800

    commit

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 const createWindow = () => {
-	let window = new BrowserWindow({
-		width: 1920,
-		height: 1080,
-		webPreference: true
-	})
-	window.loadFile('index.html')
-	window.webContents.openDevTools()
-	window.on('closed', () => {
-		window = null
-	})
-	
+    let window = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreference: true
+    })
+    // window.loadFile('http://173.100.1.70:8080')
+    window.loadFile('index.html')
+    window.webContents.openDevTools()
+    window.on('closed', () => {
+        window = null
+    })
+
 }
+
+ipcMain.on('getPrinterList', (event) => {
+    //在主线程中获取打印机列表
+    const list = mainWindow.webContents.getPrinters();
+    //通过webContents发送事件到渲染线程，同时将打印机列表也传过去
+    mainWindow.webContents.send('getPrinterList', list);
+});
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-	// if (process.)
+    // if (process.)
 })
 
 app.on('activate', () => {
-  if (window === null) {
-    createWindow()
-  }
+    if (window === null) {
+        createWindow()
+    }
 })
